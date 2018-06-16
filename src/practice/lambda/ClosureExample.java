d6a2a28cc638406a26addda27269b554daff593d
mehdi <email2mehdihasan@gmail.com>
2018-06-16 21:02:32 +0600

    - closure in lambda

---
+++ src/practice/lambda/ClosureExample.java
@@ -0,0 +1,18 @@
+package practice.lambda;
+
+public class ClosureExample {
+
+	public static void main(String[] args) {
+		int i = 10;
+		int j = 20; // effectively final
+		doProcess(i, p -> System.out.println(i + j));
+	}
+	
+	private static void doProcess(int i, Process p) {
+		p.process(i);
+	}
+}
+
+interface Process {
+	void process(int i);
+}
