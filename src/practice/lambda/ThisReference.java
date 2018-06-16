22aca6fa3bce28f3193f8a93de8b3dd2ba2bfd6f
mehdi <email2mehdihasan@gmail.com>
2018-06-16 21:03:02 +0600

    - this reference in lambda

---
+++ src/practice/lambda/ThisReference.java
@@ -0,0 +1,24 @@
+package practice.lambda;
+
+public class ThisReference {
+	
+	public void doProcess(int i, Process p) {
+		p.process(i);
+	}
+	
+	public void execute() {
+		doProcess(10, i -> {
+			System.out.println("Value of i is :" + i);
+			System.out.println(this);
+		});
+	}
+
+	public static void main(String[] args) {
+		ThisReference thisReference = new ThisReference();
+		thisReference.execute();
+	}
+	
+	public String toString() {
+		return "This is reference of ThisReference";
+	}
+}
