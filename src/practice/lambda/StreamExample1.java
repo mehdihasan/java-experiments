9fa3ca30d4f61d97cf280ec9e32e21204e4defed
mehdi <email2mehdihasan@gmail.com>
2018-06-18 20:53:03 +0600

    - java 8 stream example

---
+++ src/practice/lambda/StreamExample1.java
@@ -0,0 +1,27 @@
+package practice.lambda;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class StreamExample1 {
+
+	public static void main(String[] args) {
+		List<Animal> animals = new ArrayList<>();
+		animals.add(new Animal("fish", false, true));
+		animals.add(new Animal("kangaroo", true, false));
+		animals.add(new Animal("rabbit", true, false));
+		animals.add(new Animal("turtle", false, true));
+		animals.add(new Animal("frog", true, true));
+		animals.add(new Animal("tiger", true, true));
+		animals.add(new Animal("human", true, true));
+		
+		animals.stream()
+			.filter(a -> a.canSwim())
+			.forEach(System.out::println); // this is called method reference // same as (a) -> System.out.println(a)
+		
+		System.out.println("Total item: " + animals.parallelStream()
+			.filter(a -> a.canSwim())
+			.count());
+	}
+
+}
