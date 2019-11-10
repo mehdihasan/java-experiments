60300cc195a5d891f77919e2a99444ec34794b26
Mehdi Hasan <email2mehdihasan@gmail.com>
2019-11-10 09:07:51 +0100

    started rx java

--- src/main/java/me/mh/rxjava/TestOne.java
+++ src/main/java/me/mh/rxjava/TestOne.java
@@ -1,8 +1,29 @@
 package me.mh.rxjava;
 
+import io.reactivex.*;
+
 public class TestOne {
 
     public static void main(String[] args) {
         System.out.println("RX Java");
+
+        Observable<String> hello = Observable.just("Howdy!");
+        Observable<String> howdy = Observable.just("Howdy!");
+        howdy.subscribe(System.out::println);
+        Observable.just("Hello", "World")
+          .subscribe(System.out::println);
+          
+        
+        List<String> words = Arrays.asList(
+            "the",
+            "quick",
+            "brown",
+            "fox",
+            "jumped",
+            "over",
+            "the",
+            "lazy",
+            "dog"
+            );
     }
 }
